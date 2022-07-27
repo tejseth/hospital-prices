046a486d1b468fa34c675485cf3651568a5a82b3
tejseth <tejseth@umich.edu>
2022-07-27 17:35:43 -0400

    binding the 30 data frames together

--- 1-loading-data.R
+++ 1-loading-data.R
@@ -122,22 +122,26 @@ select_specialty_flint <- read_csv("Price Transparency Project/38-3329100_Select
 select_specialty_grosse_pointe <- read_csv("Price Transparency Project/38-3345654_Select_Specialty_Hospital-Grosse_Pointe_standardcharges.csv") |> 
   mutate(hospital_code = "38_3345654",
          hospital_name = "Select Specialty Hospital",
-         city = "Grosse Pointe")
+         city = "Grosse Pointe") |> 
+  dplyr::select(-PHARMACY_NDC)
 
 select_specialty_macomb <- read_csv("Price Transparency Project/38-3345654_Select_Specialty_Hospital-Macomb_standardcharges.csv") |> 
   mutate(hospital_code = "38_3345654",
          hospital_name = "Select Specialty Hospital",
-         city = "Macomb")
+         city = "Macomb") |> 
+  dplyr::select(-PHARMACY_NDC)
 
 select_specialty_pontiac <- read_csv("Price Transparency Project/38-3345654_Select_Specialty_Hospital-Pontiac_standardcharges.csv") |> 
   mutate(hospital_code = "38_3345654",
          hospital_name = "Select Specialty Hospital",
-         city = "Pontiac")
+         city = "Pontiac") |> 
+  dplyr::select(-PHARMACY_NDC)
 
 select_specialty_ann_arbor <- read_csv("Price Transparency Project/38-3389548_Select_Specialty_Hospital-Ann_Arbor_standardcharges.csv") |> 
   mutate(hospital_code = "38_3389548",
          hospital_name = "Select Specialty Hospital",
-         city = "Ann Arbor")
+         city = "Ann Arbor") |> 
+  dplyr::select(-PHARMACY_NDC)
 
 # McLaren Caro Community Hospital can't be read in
 # Michigan Medicine can't be a XML (file is too big)
@@ -148,7 +152,8 @@ select_specialty_ann_arbor <- read_csv("Price Transparency Project/38-3389548_Se
 select_specialty_battle_creek <- read_csv("Price Transparency Project/75-1962822_Select_Specialty_Hospital-Battle_Creek_standardcharges.csv") |> 
   mutate(hospital_code = "75_1962822",
          hospital_name = "Select Specialty Hospital",
-         city = "Battle Creek")
+         city = "Battle Creek") |> 
+  dplyr::select(-PHARMACY_NDC)
 
 # 101000 Parent HFHS Henry Ford can't be a XML
 # 103000 Parent HFHS Kingswood can't be a XML
@@ -177,9 +182,10 @@ beaumont_dearborn <- beaumont_dearborn[-1,]
 beaumont_dearborn <- beaumont_dearborn |>
   mutate(hospital_code = "381405141",
          hospital_name = "Beaumont",
-         city = "Dearborn") |>
+         city = "Dearborn", 
+         CPT_CODE = "") |>
   dplyr::select(CHARGE_CODE = CODE, CHARGE_DESCRIPTION = PROCEDURE_DESCRIPTION, 
-                CHARGE = `GROSS CHARGE`, REVENUE_CODE = REV_CODE, hospital_name,
+                CHARGE = `GROSS CHARGE`, REVENUE_CODE = REV_CODE, CPT_CODE, hospital_code,
                 hospital_name, city)
 
 
@@ -189,9 +195,10 @@ beaumont_taylor <- beaumont_taylor[-1,]
 beaumont_taylor <- beaumont_taylor |>
   mutate(hospital_code = "381405141",
          hospital_name = "Beaumont",
-         city = "Taylor") |> 
+         city = "Taylor", 
+         CPT_CODE = "") |> 
   dplyr::select(CHARGE_CODE = CODE, CHARGE_DESCRIPTION = PROCEDURE_DESCRIPTION, 
-                CHARGE = `GROSS CHARGE`, REVENUE_CODE = REV_CODE, hospital_name,
+                CHARGE = `GROSS CHARGE`, REVENUE_CODE = REV_CODE, CPT_CODE, hospital_code,
                 hospital_name, city)
 
 # beaumont_trenton <- read_csv("Price Transparency Project/381405141_beaumont-hospital-trenton_hospital-standardcharges.csv") |> 
@@ -216,7 +223,110 @@ beaumont_taylor <- beaumont_taylor |>
 #                 CHARGE = `GROSS CHARGE`, REVENUE_CODE = REV_CODE, hospital_name,
 #                 hospital_name, city)
 
-X381426919_beaumont_hospital_farmington_hills_hospital_standardcharges <- read_csv("Price Transparency Project/381426919_beaumont-hospital-farmington-hills_hospital-standardcharges.csv")
+beaumont_farmington_hills <- read_csv("Price Transparency Project/381426919_beaumont-hospital-farmington-hills_hospital-standardcharges.csv")
+names(beaumont_farmington_hills) <- beaumont_farmington_hills[1,]
+beaumont_farmington_hills <- beaumont_farmington_hills[-1,]
+beaumont_farmington_hills <- beaumont_farmington_hills |> 
+  mutate(hospital_code = "381426919",
+         hospital_name = "Beaumont",
+         city = "Farmington Hills",
+         CPT_CODE = "") |> 
+  dplyr::select(CHARGE_CODE = CODE, CHARGE_DESCRIPTION = PROCEDURE_DESCRIPTION, 
+                CHARGE = `GROSS CHARGE`, REVENUE_CODE = REV_CODE, CPT_CODE, hospital_code,
+                hospital_name, city) 
 
+midmichigan_gratiot <- read_csv("Price Transparency Project/381437919_midmichigan-medical-center-gratiot_standardcharges.csv")
+names(midmichigan_gratiot) <- midmichigan_gratiot[11,]
+midmichigan_gratiot <- midmichigan_gratiot[-11,]
+midmichigan_gratiot <- midmichigan_gratiot |> 
+  dplyr::select(CHARGE_CODE = `CDM Charge Code`, CHARGE_DESCRIPTION = `Charge / Procedure / Supply Code Description`,
+                CHARGE = `Gross Charge`, REVENUE_CODE = `Revenue Code`, CPT_CODE = `Code (CPT / HCPCS / MS-DRG)`) |> 
+  mutate(hospital_code = "381437919",
+         city = "Alma",
+         hospital_name = "MidMichigan Medical Center - Gratiot")
 
+beaumont_grosse_pointe <- read_csv("Price Transparency Project/381459362_beaumont-hospital-grosse-pointe_hospital-standardcharges.csv")
+names(beaumont_grosse_pointe) <- beaumont_grosse_pointe[1,]
+beaumont_grosse_pointe <- beaumont_grosse_pointe[-1,]
+beaumont_grosse_pointe <- beaumont_grosse_pointe |> 
+  dplyr::select(CHARGE_CODE = Code, CHARGE_DESCRIPTION = `Procedure Description`, 
+              CHARGE = `GROSS CHARGE`, REVENUE_CODE = REV_CODE) |> 
+  mutate(CPT_CODE = "",
+         hospital_code = "381459362",
+         hospital_name = "Beaumont",
+         city = "Grosse Pointe")
 
+beaumont_royal_oak <- read_csv("Price Transparency Project/381459362_beaumont-hospital-royal-oak_hospital-standardcharges.csv")
+names(beaumont_royal_oak) <- beaumont_royal_oak[1,]
+beaumont_royal_oak <- beaumont_royal_oak[-1,]
+beaumont_royal_oak <- beaumont_royal_oak |> 
+  mutate(hospital_code = "381459362",
+         hospital_name = "Beaumont",
+         city = "Royal Oak",
+         CPT_CODE = "") |> 
+  dplyr::select(CHARGE_CODE = CODE, CHARGE_DESCRIPTION = PROCEDURE_DESCRIPTION, 
+                CHARGE = `GROSS CHARGE`, REVENUE_CODE = REV_CODE, CPT_CODE, hospital_code,
+                hospital_name, city) 
+
+beaumont_troy <- read_csv("Price Transparency Project/381459362_beaumont-hospital-troy_hospital-standardcharges.csv")
+names(beaumont_troy) <- beaumont_troy[1,]
+beaumont_troy <- beaumont_troy[-1,]
+beaumont_troy <- beaumont_troy |> 
+  mutate(hospital_code = "381459362",
+         hospital_name = "Beaumont",
+         city = "Troy",
+         CPT_CODE = "") |> 
+  dplyr::select(CHARGE_CODE = CODE, CHARGE_DESCRIPTION = PROCEDURE_DESCRIPTION, 
+                CHARGE = `GROSS CHARGE`, REVENUE_CODE = REV_CODE, CPT_CODE, hospital_code,
+                hospital_name, city) 
+
+midmichigan_clare <- read_csv("Price Transparency Project/381518643_midmichigan-medical-center-clare_standardcharges.csv")
+names(midmichigan_clare) <- midmichigan_clare[11,]
+midmichigan_clare <- midmichigan_clare[-11,]
+midmichigan_clare <- midmichigan_clare |> 
+  dplyr::select(CHARGE_CODE = `CDM Charge Code`, CHARGE_DESCRIPTION = `Charge / Procedure / Supply Code Description`,
+                CHARGE = `Gross Charge`, REVENUE_CODE = `Revenue Code`, CPT_CODE = `Code (CPT / HCPCS / MS-DRG)`) |> 
+  mutate(hospital_code = "381437919",
+         city = "Clare",
+         hospital_name = "MidMichigan Medical Center")
+
+midmichigan_alpena <- read_csv("Price Transparency Project/386000029_midmichigan-medical-center-alpena_standardcharges.csv")
+names(midmichigan_alpena) <- midmichigan_alpena[11,]
+midmichigan_alpena <- midmichigan_alpena[-11,]
+midmichigan_alpena <- midmichigan_alpena |> 
+  dplyr::select(CHARGE_CODE = `CDM Charge Code`, CHARGE_DESCRIPTION = `Charge / Procedure / Supply Code Description`,
+                CHARGE = `Gross Charge`, REVENUE_CODE = `Revenue Code`, CPT_CODE = `Code (CPT / HCPCS / MS-DRG)`) |> 
+  mutate(hospital_code = "386000029",
+         city = "Alpena",
+         hospital_name = "MidMichigan Medical Center")
+
+midmichigan_gladwin <- read_csv("Price Transparency Project/386020434_midmichigan-medical-center-gladwin_standardcharges.csv")
+names(midmichigan_gladwin) <- midmichigan_gladwin[11,]
+midmichigan_gladwin <- midmichigan_gladwin[-11,]
+midmichigan_gladwin <- midmichigan_gladwin |> 
+  dplyr::select(CHARGE_CODE = `CDM Charge Code`, CHARGE_DESCRIPTION = `Charge / Procedure / Supply Code Description`,
+                CHARGE = `Gross Charge`, REVENUE_CODE = `Revenue Code`, CPT_CODE = `Code (CPT / HCPCS / MS-DRG)`) |> 
+  mutate(hospital_code = "386020434",
+         city = "Gladwin",
+         hospital_name = "MidMichigan Medical Center")
+
+midmichigan_west_branch <- read_csv("Price Transparency Project/464088182_midmichigan-medical-center-west-branch_standardcharges.csv")
+names(midmichigan_west_branch) <- midmichigan_west_branch[11,]
+midmichigan_west_branch <- midmichigan_west_branch[-11,]
+midmichigan_west_branch <- midmichigan_west_branch |> 
+  dplyr::select(CHARGE_CODE = `CDM Charge Code`, CHARGE_DESCRIPTION = `Charge / Procedure / Supply Code Description`,
+                CHARGE = `Gross Charge`, REVENUE_CODE = `Revenue Code`, CPT_CODE = `Code (CPT / HCPCS / MS-DRG)`) |> 
+  mutate(hospital_code = "464088182",
+         city = "West Branch",
+         hospital_name = "MidMichigan Medical Center")
+
+binded <- do.call("rbind", list(beaumont_dearborn, beaumont_farmington_hills, beaumont_grosse_pointe,
+                                beaumont_royal_oak, beaumont_taylor, beaumont_troy, karamanos_cancer_institute,
+                                mclaren_bay_region, mclaren_bay_special_care, mclaren_central, mclaren_flint,
+                                mclaren_greater_lansing, mclaren_lapeer, mclaren_macomb, mclaren_northern_michigan,
+                                mclaren_port_huron, midmichigan_alpena, midmichigan_clare, midmichigan_gladwin, 
+                                midmichigan_gratiot, midmichigan_west_branch, select_specialty_ann_arbor,
+                                select_specialty_battle_creek, select_specialty_flint, select_specialty_grosse_pointe, 
+                                select_specialty_hospital_northwest_detroit, select_specialty_hospital_saginaw,
+                                select_specialty_hospital_wyandotte, select_specialty_hospital_wyandotte,
+                                select_specialty_macomb, select_specialty_pontiac))
